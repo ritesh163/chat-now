a1682c4df999c08a0dbc2d420404afa4a095ea70
ritesh <riteshyuvasoft163@gmail.com>
2017-09-18 18:58:52 +0530

    add user mailer

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,17 @@ class RegistrationsController < Devise::RegistrationsController
 	def new
     super
   end
-
+  
   def create
   	@user = User.new(user_params)
-  	@user.save!
-  	redirect_to root_path   
+  	if @user.save
+      UserMailer.registration_confirmation(@user).deliver
+      flash[:success] = "Please confirm your email address to continue"
+      redirect_to root_url
+    else
+      flash[:error] = "Ooooppss, something went wrong!"
+      render 'new'
+    end  
   end
 
   def update
